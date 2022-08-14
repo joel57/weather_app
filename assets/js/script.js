278595e5995b421536f212e59b9a1f449c692161
joel57 <67278919+joel57@users.noreply.github.com>
2022-08-14 08:00:56 -0700

    created the citys varibles and  some functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// Global variable 
+var cityList = [];
+var cityname;
+
+// local storage functions
+initCityList();
+initWeather();
+
+
+// This function displays the city entered by the user into the DOM
+function renderCities(){
+    $("#cityList").empty();
+    $("#cityInput").val("");
+    
+    for (i=0; i<cityList.length; i++){
+        var a = $("<a>");
+        a.addClass("list-group-item list-group-item-action list-group-item-primary city");
+        a.attr("data-name", cityList[i]);
+        a.text(cityList[i]);
+        $("#cityList").prepend(a);
+    } 
+}
+
+// This function pulls the city list array from local storage
+function initCityList() {
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+    
+    if (storedCities !== null) {
+        cityList = storedCities;
+    }
+    
+    renderCities();
+    }
