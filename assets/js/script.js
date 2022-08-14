fb5b8c3f2282a880ae1b05064e7853b15428a689
joel57 <67278919+joel57@users.noreply.github.com>
2022-08-14 16:51:01 -0700

    added js buttons handlers for the citys, added local storage as well, functions and debbuged

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,55 @@ function initCityList() {
     
     renderCities();
     }
+// This function pull the current city into local storage to display the current weather forecast on reload
+function initWeather() {
+    var storedWeather = JSON.parse(localStorage.getItem("currentCity"));
+
+    if (storedWeather !== null) {
+        cityname = storedWeather;
+
+        displayWeather();
+        displayFiveDayForecast();
+    }
+}
+
+// This function saves the city array to local storage
+function storeCityArray() {
+    localStorage.setItem("cities", JSON.stringify(cityList));
+    }
+
+// This function saves the currently display city to local storage
+function storeCurrentCity() {
+
+    localStorage.setItem("currentCity", JSON.stringify(cityname));
+}
+      
+
+// Click event handler for city search button
+$("#citySearchBtn").on("click", function(event){
+    event.preventDefault();
+
+    cityname = $("#cityInput").val().trim();
+    if(cityname === ""){
+        alert("Please enter a city to look up")
+
+    }else if (cityList.length >= 5){  
+        cityList.shift();
+        cityList.push(cityname);
+
+    }else{
+    cityList.push(cityname);
+    }
+    storeCurrentCity();
+    storeCityArray();
+    renderCities();
+    displayWeather();
+    displayFiveDayForecast();
+});
+
+// Event handler for if the user hits enter after entering the city search term
+$("#cityInput").keypress(function(e){
+    if(e.which == 13){
+        $("#citySearchBtn").click();
+    }
+})
